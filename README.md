c5e957193d44c0b68744b22f78c8d5a321a2fb94
nssmmn <nssmmn@gmail.com>
2015-09-23 11:53:57 +0100

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+![Sans-accent Logo](/sans-accent-logo.png)
+# sans-accent
+Remove french accents in text.
+
+## Usage
+```js
+var sansAccent = require('sans-accent')();
+
+var text = "Combien ça coûte un œuf?";
+
+sansAccent(text);  // return "Combien ca coute un oeuf?"
+```
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports={
-  
+//source : http://fr.wikipedia.org/wiki/Diacritiques_utilis%C3%A9s_en_fran%C3%A7ais
+w = "àâäçéèêëîïôöùûüÿÀÂÄÇÉÈÊËÎÏÔÖÙÛÜŸ".split("");
+w.push("Œ","œ");
+wo = "aaaceeeeiioouuuyAAACEEEEIIOOUUUY".split("");
+wo.push("OE","oe");
+
+function _sansAccent(text){
+  for(var i=0 ; i< w.length ; i++){
+    text = text.replace( new RegExp(w[i],"g") , wo[i]);
+  }
+  return text;
+}
+
+module.exports = function(){
+  return _sansAccent;
 };
